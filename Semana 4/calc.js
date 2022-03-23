4e6b974552069b2fe536f7a8af1ca08fc06faa76
Pablo Jacinto <pablojacintopsi@gmail.com>
2022-03-23 13:00:22 -0300

    Exercício 3

---
+++ Semana 4/calc.js
@@ -0,0 +1,71 @@
+function calculadora() {
+   let n1 = parseFloat(prompt('Digite o primeiro número'));
+   let n2 = parseFloat(prompt('Digite o segundo número'));
+   let expressao = prompt('Digite a expressão');
+   let resultado;
+
+   function calculadora(n1, n2, expressao) {
+      switch (expressao) {
+         case '+':
+            resultado = n1 + n2;
+            //TENTEI E NÃO DEU CERTO TAMBÉM alert(resultado);
+            break;
+
+         case '-':
+            resultado = n1 - n2;
+            break;
+
+         case '/':
+            resultado = n1 / n2;
+            break;
+
+         case '*':
+            resultado = n1 * n2;
+            break;
+
+         default:
+            resultado = 'não é uma expressão';
+      }
+
+      return resultado;
+   }
+
+   //chamando a função
+   calculadora();
+
+   alert(resultado);
+}
+
+// outro teste
+
+function exercicio_2() {
+   let tipoCalculo = 1;
+   let primeiroValor = 2;
+   let segundoValor = 2;
+   let soma = 0;
+   tipoCalculo = prompt(
+      'Qual tipo de calculo deseja fazer? soma(1), subtração ( 2 ) , multiplicação ( 3 ), Divisão ( 4 )'
+   );
+   primeiroValor = prompt('Digite o primeiro valor para o calculo: ');
+   segundoValor = prompt('Digite o segundo valor para o calculo: ');
+   switch (tipoCalculo) {
+      case '1':
+         soma = parseInt(primeiroValor) + parseInt(segundoValor);
+         console.log(soma);
+         break;
+      case '2':
+         soma = parseInt(primeiroValor) - parseInt(segundoValor);
+         console.log(soma);
+         break;
+      case '3':
+         soma = parseInt(primeiroValor) * parseInt(segundoValor);
+         console.log(soma);
+         break;
+      case '4':
+         soma = parseFloat(primeiroValor) / parseFloat(segundoValor);
+         console.log(soma);
+         break;
+      default:
+         console.log('Opção Invalida');
+   }
+}
---
+++ Semana 4/parouimpar.js
@@ -0,0 +1,8 @@
+function parouimpar() {
+   let num = parseInt(prompt('Digite um número inteiro'));
+
+   num % 2 == 0 ? alert('É número par') : alert('É número ímpar');
+
+   //Será que funciona?
+   //    let num = Number(prompt('Digite um número inteiro')) % 2 != 0 ? 'É número par' : 'É número ímpar';
+}
