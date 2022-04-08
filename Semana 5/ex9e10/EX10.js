f828266b70e166f6511c17e46274ea87bd20df97
Pablo Jacinto <pablojacintopsi@gmail.com>
2022-04-08 09:57:10 -0300

    Feito exercício de promises

--- Semana 5/ex9e10/EX10.js
+++ Semana 5/ex9e10/EX10.js
@@ -15,3 +15,33 @@ function sleep(valor) {
 
 //chamar a função
 sleep(3);
+
+///OUTRA FORMA DE FAZER
+
+const meuSleep = new Promise((resolve, reject) => {
+   setTimeout(() => {
+      let error = false;
+
+      if (!error) {
+         resolve(`Tempo de sleep cinco segundos`);
+         // resolve(console.log(`Tempo de sleep cinco segundos`));
+      } else {
+         reject(console.log('TEVE ERRO'));
+      }
+   }, 5000);
+});
+
+// //chamar a promise.
+meuSleep.then((respostaDaPromise) => {
+   console.log(respostaDaPromise);
+});
+
+// //chamar a promise encadeando o then.
+
+meuSleep
+   .then((resposta) => {
+      return resposta.toUpperCase();
+   })
+   .then((stringmodificado) => {
+      console.log(stringmodificado);
+   });
