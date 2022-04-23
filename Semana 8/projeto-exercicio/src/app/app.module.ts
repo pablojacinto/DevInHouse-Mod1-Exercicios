998fec4f755778f604a1ed9f52928860f182e57b
Pablo Jacinto <pablojacintopsi@gmail.com>
2022-04-23 09:01:04 -0300

    Ajustes

--- Semana 8/projeto-exercicio/src/app/app.module.ts
+++ Semana 8/projeto-exercicio/src/app/app.module.ts
@@ -2,21 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ContentComponent } from './content/content.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContentComponent } from './components/content/content.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { InformacoesComponent } from './components/informacoes/informacoes.component';
 
+//importando o roteamento//
+import { RouterModule } from '@angular/router';
+//////////////////////////
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ContentComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    InformacoesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    //importanto o routermodule - https://angular.io/guide/router-tutorial
+    RouterModule.forRoot([
+      { path: 'about', component: AboutComponent },
+      { path: '', component: HomeComponent },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
